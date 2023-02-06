5ab81e0b180b857dbe7ba3f32b90bd1870ef4e03
francis-devbr <francis-devbr@outlook.com>
2023-02-06 10:38:34 -0300

    add soulbar

---
+++ src/Components/Game/PlayerSoulBar/PlayerSoulBar.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+
+const Root = styled.div`
+  position: fixed;
+  bottom: -50px;
+  left: 50%;
+  transform: translateX(-50%);
+  --cardWidth: var(--cardWidthBigger);
+`;
+
+export default function PlayerSoulBar() {
+ 
+  return (
+    <Root>
+      
+    </Root>
+  );
+}
